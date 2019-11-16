6252ca7b0ff9651e068154ebebe9e0234592473c
Ruben Mora Vargas <rmoravargas@hotmail.com>
2019-11-15 22:34:35 -0600

    Se agrega una función para que muestre cíclicamente interacciones de la aplicación

---
+++ dadoCsharp/dadoCsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace dadoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConsoleKeyInfo keyInfo;
+
+            Console.WriteLine("Bienvenido a la aplicación Dado en C#." + Environment.NewLine);
+            Console.WriteLine("Presione Escape para terminar la aplicación" + Environment.NewLine);
+            Console.WriteLine("Presione Enter para ver una nueva visualización." + Environment.NewLine);
+            Console.WriteLine("--------------------------------------" + Environment.NewLine);
+
+            do
+            {
+
+                while (!Console.KeyAvailable)
+                { }
+                keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Has solicitado una Nueva Visualización");
+                }
+
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Aplicación finalizada." + Environment.NewLine);
+            Environment.Exit(0);
+        }
+    }
+}
