e31521643f2a3876908e40815aff1d3819fb4876
Ruben Mora Vargas <rmoravargas@hotmail.com>
2019-11-16 10:16:53 -0600

    Se agrega la funcionalidad para dibujar el dado en pantalla

--- dadoCsharp/dadoCsharp/Program.cs
+++ dadoCsharp/dadoCsharp/Program.cs
@@ -5,15 +5,36 @@ namespace dadoCsharp
 {
     class Program
     {
+        static void DibujarDado(int caraSuperior, int caraFrontal, int caraIzquierda)
+        {
+
+            //cara superior
+            Console.WriteLine("                    ###########");
+            Console.WriteLine("                    #         #");
+            Console.WriteLine("                    #    {0}    #", caraSuperior);
+            Console.WriteLine("                    #         #"); 
+            //caras frontales 
+            Console.WriteLine("#########################################");
+            Console.WriteLine("#         #         #         #         #");
+            Console.WriteLine("#    {0}    #    {1}    #    {2}    #    {3}    #",7-caraFrontal,7- caraIzquierda , caraFrontal, caraIzquierda);
+            Console.WriteLine("#         #         #         #         #");
+            Console.WriteLine("#########################################");
+            //cara inferior   
+            Console.WriteLine("                    #         #");
+            Console.WriteLine("                    #    {0}    #", 7-caraSuperior);
+            Console.WriteLine("                    #         #");
+            Console.WriteLine("                    ###########");
+ 
+        }
         static void Main(string[] args)
         {
 
             ConsoleKeyInfo keyInfo;
 
-            Console.WriteLine("Bienvenido a la aplicación Dado en C#." + Environment.NewLine);
-            Console.WriteLine("Presione Escape para terminar la aplicación" + Environment.NewLine);
-            Console.WriteLine("Presione Enter para ver una nueva visualización." + Environment.NewLine);
-            Console.WriteLine("--------------------------------------" + Environment.NewLine);
+            Console.WriteLine("Bienvenido a la aplicación Dado en C#." );
+            Console.WriteLine("Presione Escape para terminar la aplicación");
+            Console.WriteLine("Presione Enter para ver una nueva visualización.");
+            Console.WriteLine("--------------------------------------");
 
             do
             {
@@ -24,7 +45,8 @@ namespace dadoCsharp
 
                 if (keyInfo.Key == ConsoleKey.Enter)
                 {
-                    Console.WriteLine("Has solicitado una Nueva Visualización");
+                    Console.WriteLine(Environment.NewLine +"Has solicitado una Nueva Visualización" + Environment.NewLine);
+                    DibujarDado(4, 5, 6);
                 }
 
 
